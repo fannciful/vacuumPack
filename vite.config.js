5f883a680493420d03168e92d78f8dd90f4552f3
Yuliia <fannciful@MacBook-Air---Yuliia.local>
2025-06-19 21:42:52 +0300

    change cofig settings

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,18 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/vacuumPack/',
+  base: process.env.NODE_ENV === 'production' ? '/vacuumPack/' : '/',
   plugins: [
-    tailwindcss(),
-    react()
+    react(),
+    tailwindcss()
   ],
+  server: {
+    port: 3000,
+    open: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  }
 })
